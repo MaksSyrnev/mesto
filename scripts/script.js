134465b550ba4041a01e4a797cd28690e921a06e
Maks Syrnev <msyrnev@yandex.ru>
2020-09-02 09:33:57 +1000

    исправления 2

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ let closeButton = popup.querySelector('.popup__button-close-form');
 let saveButton = popup.querySelector('.popup__button-save-form');
 let nameProfile = document.querySelector('.profile__title');
 let jobProfile = document.querySelector('.profile__subtitle');
-let inputName = popup.querySelector('.js-popup__username');
-let inputJob = popup.querySelector('.js-popup__job');
+let inputName = popup.querySelector('.popup__form-input_js_username');
+let inputJob = popup.querySelector('.popup__form-input_js_job');
 let formElement = document.querySelector('.popup__form'); // Находим форму в DOM
 
 //задекларируем функцию закрытия окна
@@ -27,16 +27,9 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                         // Так мы можем определить свою логику отправки.
                         // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let inputs = popup.querySelectorAll('input');
-    // Получите значение полей из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
     nameProfile.textContent = inputName.value;
     jobProfile.textContent = inputJob.value;
     formClose ();
-    //popup.classList.remove('popup_opened');
 }
 
 // Прикрепляем обработчик к форме:
