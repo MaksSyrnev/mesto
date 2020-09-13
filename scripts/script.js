942878eb370403c4c4d8642e36b5904a3918694a
Maks Syrnev <msyrnev@yandex.ru>
2020-09-13 17:06:14 +1000

    Добавить функционал кнопок удаление и нравится

--- scripts/script.js
+++ scripts/script.js
@@ -16,7 +16,7 @@ let inputNameCard = popupAddCard.querySelector('.popup__form-input_js_namecard')
 let inputLinkCard = popupAddCard.querySelector('.popup__form-input_js_linkcard');
 let formAddCard = popupAddCard.querySelector('.popup__form_js_addcard'); // Находим форму добавления карточки в DOM
 
-const cardTemplate = document.querySelector('#card').content; //находим шаблон картоки на странице
+const cardTemplate = document.querySelector('#card').content; //находим шаблон карточки на странице
 const elementsGallery = document.querySelector('.elements__gallery'); //контейнер для карточек
 
 //массив карточек при первом открытии страницы
@@ -55,24 +55,36 @@ cardElement.querySelector('.elements__card-img').src = item.link;
 cardElement.querySelector('.elements__card-img').alt = item.name;
 cardElement.querySelector('.elements__card-title').textContent = item.name;
 
+//вешаем обработчики кнопок карточки до добавлении
+cardElement.querySelector('.elements__button-like').addEventListener('click', function (evt) {
+  const eventTarget = evt.target;
+  eventTarget.classList.toggle('elements__button-like_checked');
+  });
+
+cardElement.querySelector('.elements__button-del').addEventListener('click', function (evt) {
+  const buttonDelTarget = evt.target;
+  const CardsItem = buttonDelTarget.closest('.elements__card');
+  CardsItem.remove();
+  });
+
 elementsGallery.append(cardElement);//добавляем карточку в контейнер-галлерею для отображания на странице
 });
 
 
-//задекларируем функцию закрытия окна профиля
+//функции формы редактирования профиля
+//закрытие окна редактирования профиля
 function formCloseEditProfile () {
   popupEditProfile.classList.remove('popup_opened');
 }
 
-//функция открытия профиля
+//открытие редактирования профиля
 function formOpenProfile () {
   popupEditProfile.classList.add('popup_opened');
   inputName.value = nameProfile.textContent; //подставляем значения из профиля в форму
   inputJob.value = jobProfile.textContent;
 }
 
-
-// Обработчик «отправки» формы
+// Обработчик «отправки» формы редактирования профиля
 function formEditProfileSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
@@ -81,7 +93,8 @@ function formEditProfileSubmitHandler (evt) {
     formCloseEditProfile();
 }
 
-//открытие и закрытие формы добавления
+//Форма добавления карточки
+//открытие и закрытие формы добавления карточки
 function formOpenCloseAddCard () {
   popupAddCard.classList.toggle('popup_opened');
 }
@@ -90,23 +103,47 @@ function formOpenCloseAddCard () {
 function formAddCardSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
-  const cardElementNew = cardTemplate.cloneNode(true);
+  const cardElementNew = cardTemplate.cloneNode(true); //клонируем и наполняем шаблон полученными значениями
   cardElementNew.querySelector('.elements__card-img').src = inputLinkCard.value;
   cardElementNew.querySelector('.elements__card-img').alt = inputNameCard.value;
   cardElementNew.querySelector('.elements__card-title').textContent = inputNameCard.value
-  elementsGallery.prepend(cardElementNew);
+  //вешаем обработчики кнопок при добавлении
+  cardElementNew.querySelector('.elements__button-like').addEventListener('click', function (evt) {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('elements__button-like_checked');
+    });
+
+  cardElementNew.querySelector('.elements__button-del').addEventListener('click', function (evt) {
+    const buttonDelTarget = evt.target;
+    const CardsItem = buttonDelTarget.closest('.elements__card');
+    CardsItem.remove();
+    });
+
+  elementsGallery.prepend(cardElementNew); //добавляем на страницу
+  //очищаем поля
+  inputLinkCard.value = '';
+  inputNameCard.value = '' ;
 
   formOpenCloseAddCard();
 }
 
-
 //обработчики событий
-
-
+//окно редактирования профиля
 formEditProfile.addEventListener('submit', formEditProfileSubmitHandler);
 editButton.addEventListener('click', formOpenProfile);
 closeButtonEditProfile.addEventListener('click', formCloseEditProfile);
 
+//окно добавление карточки
 formAddCard.addEventListener('submit', formAddCardSubmitHandler);
 addButton.addEventListener('click', formOpenCloseAddCard);
 closeButtonAddCard.addEventListener('click', formOpenCloseAddCard);
+
+
+/*
+
+  button.addEventListener('click', function () {
+    const listItem = button.closest('.elements__card');
+    listItem.remove();
+    }
+
+*/
